4650a6c19112b29c83b0939be643093d17d9c616
TreePusle <387786238@qq.com>
2016-11-12 14:24:51 +0800

    字符串最长不重复子串的长度
    Test1

---
+++ Exam1_FindMaxDifferent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class Program
+    {
+        static int FindMaxDifferent(string s)
+        {
+           List<char> different = new List<char>();
+            char[] sChar = s.ToArray();
+            int max = 0,t=0;
+            for (int i=0; i < s.Length; i++)
+            {
+                if (!different.Contains(sChar[i]))
+                {
+                    different.Add(sChar[i]);
+                    t++;
+                }
+                else
+                {
+                    if (t > max)
+                        max = t;
+                    t = 0;
+                    different.Clear();
+                }
+            }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindMaxDifferent("abcabcbb"));
+            Console.ReadLine();
+        }
+    }
+}
